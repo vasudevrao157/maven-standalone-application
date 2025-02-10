c1150336e58aae821ca7bcd815cd093d5305a76c
vasudevrao157 <vasudevarao179@gmail.com>
2025-02-10 12:29:26 +0530

    Update HelloWorld.java

--- src/main/java/com/mt/sample/HelloWorld.java
+++ src/main/java/com/mt/sample/HelloWorld.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Welcome to Maven Build Tool !! Today Date is:  " + getLocalCurrentDate());
+		System.out.println("Hello Good day, I'm Vasudev, Welcome to Maven Build Tool !! Today Date is:  " + getLocalCurrentDate());
 
 	}
 
